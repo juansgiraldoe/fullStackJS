14ca608cada57745fc5805ba7f95e720178d341a
Juan Sebastián Giraldo <90337681+juansgiraldoe@users.noreply.github.com>
2024-03-25 10:48:07 -0500

    [Update] - Install express. // Initialize and test express. // Install nodemon. (Backend).

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log(`Desde node.js`);
+import express from "express";
+//--------------------------------
+
+const app = express();
+app.use(`/`, ( req, res ) =>{
+  res.send(`Hola mundo.`)
+})
+
+app.listen(4000, ()=> {
+  console.log(`Servidor funcionando en el puerto 4000.`);
+});
