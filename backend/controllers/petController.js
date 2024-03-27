29fbe0d3ab907069cb1ad0caafc40b1d6408d159
Juan Sebastián Giraldo <90337681+juansgiraldoe@users.noreply.github.com>
2024-03-27 15:35:05 -0500

    [Update] - Update patient PUT feature. (Backend).

--- backend/controllers/petController.js
+++ backend/controllers/petController.js
@@ -27,18 +27,44 @@ const obtenerPaciente = async ( req, res ) => {
   const { id } = req.params
   const paciente =  await Paciente.findById(id);
 
+  if (!paciente) {
+    return res.status(404).json({msg: `No encontrado`})
+  };
+
   if ( paciente.veterinario._id.toString() !== req.veterinario._id.toString() ) {
     return res.status(403).json({msg: `Accion no valida, no tienes permiso para ver este contenido.`});
   };
-
-  if (paciente) {
-    res.json(paciente);
-  };
-
+  
+  res.json(paciente);
+  
 };
 
 const actualizarPaciente = async ( req, res ) => {
+  const { id } = req.params
+  const paciente =  await Paciente.findById(id);
+
+  if (!paciente) {
+    return res.status(404).json({msg: `No encontrado`})
+  };
   
+  if ( paciente.veterinario._id.toString() !== req.veterinario._id.toString() ) {
+    return res.status(403).json({msg: `Accion no valida, no tienes permiso para ver este contenido.`});
+  };
+  
+  //Actualizar paciente.
+  paciente.nombre = req.body.nombre || paciente.nombre;
+  paciente.propietario = req.body.propietario || paciente.propietario;
+  paciente.email = req.body.email || paciente.email;
+  paciente.fecha = req.body.fecha || paciente.fecha;
+  paciente.sintomas = req.body.sintomas || paciente.sintomas;
+  try {
+    const pacienteActualziado = await paciente.save();
+    return res.json(pacienteActualziado);
+  } catch (error) {
+    const e = new Error('Hubo un error al actualizar el paciente.');
+    res.status(404).json({msg: e.message});
+  };
+
 };
 
 const eliminarPaciente = async ( req, res ) => {
